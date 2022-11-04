9246676180558ad49a721ed9b8d9b5b4b1a8855a
salsabeelnoor <salsabeelnoor@gmail.com>
2022-11-04 20:17:13 +0600

    first commit

---
+++ src/Pages/Home/Services/Services.js
@@ -0,0 +1,31 @@
+import React, { useEffect, useState } from "react";
+import ServiceCard from "./ServiceCard";
+
+const Services = () => {
+  const [services, setServices] = useState([]);
+
+  useEffect(() => {
+    fetch("services.json")
+      .then((res) => res.json())
+      .then((data) => setServices(data));
+  }, []);
+  return (
+    <div>
+      <div className="text-center mb-6">
+        <p className="text-2xl font-bold text-orange-600">Services</p>
+        <p className="text-5xl font-semibold">Our Service Area</p>
+        <p className="text-gray-500 my-3">
+          the majority have suffered alteration in some form, by injected
+          humour, or randomised words which don't look even slightly believable.{" "}
+        </p>
+      </div>
+      <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-6">
+        {services.map((service) => (
+          <ServiceCard key={service._id} service={service}></ServiceCard>
+        ))}
+      </div>
+    </div>
+  );
+};
+
+export default Services;
