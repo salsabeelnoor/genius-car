91a2e5fab75d3d4c75cc57ddfec0e2edfb60eb7b
salsabeelnoor <salsabeelnoor@gmail.com>
2022-11-16 19:11:14 +0600

    bonus

--- src/Pages/Home/Services/Services.js
+++ src/Pages/Home/Services/Services.js
@@ -1,14 +1,26 @@
 import React, { useEffect, useState } from "react";
+import { useRef } from "react";
 import ServiceCard from "./ServiceCard";
 
 const Services = () => {
   const [services, setServices] = useState([]);
+  const [isAsc, setIsAsc] = useState(true);
+  const [search, setSearch] = useState("");
+  const searchRef = useRef();
 
   useEffect(() => {
-    fetch("http://localhost:5000/services")
+    fetch(
+      `http://localhost:5000/services?search=${search}&order=${
+        isAsc ? "asc" : "desc"
+      }`
+    )
       .then((res) => res.json())
       .then((data) => setServices(data));
-  }, []);
+  }, [isAsc, search]);
+
+  const handleSearch = () => {
+    setSearch(searchRef.current.value);
+  };
   return (
     <div>
       <div className="text-center mb-6">
@@ -18,6 +30,11 @@ const Services = () => {
           the majority have suffered alteration in some form, by injected
           humour, or randomised words which don't look even slightly believable.{" "}
         </p>
+        <input ref={searchRef} type="text" className="input input-sm" />{" "}
+        <button onClick={handleSearch}>Search</button>
+        <button onClick={() => setIsAsc(!isAsc)} className="btn btn-ghost">
+          {isAsc ? "desc" : "asc"}
+        </button>
       </div>
       <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-6">
         {services.map((service) => (
